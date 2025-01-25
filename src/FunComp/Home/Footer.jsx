1ca00d9b9abb7d8a5b3c0863924d13c452b03530
Deadshot-45 <sahumayank8179@gmail.com>
2025-01-25 20:03:56 +0530

    Footer Link added

--- src/FunComp/Home/Footer.jsx
+++ src/FunComp/Home/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
             <footer className='w-full my-12 xsm: flex items-center justify-center'>
                 <div className="flex justify-between w-full text-sm xsm:text-[10px] max-xsm:text-[10px] xsm:items-center md:text-[15px] text-zinc-400">
                     <div className="">
-                        <p>Copyright @2025, <a href="#" className='text-orange-600'>Mayank</a> All Rights Reserved.</p>
+                        <p>Copyright @2025, <a href="/" className='text-orange-600'>Mayank</a> All Rights Reserved.</p>
                     </div>
                     <div>
                         <p className="text-end">Crafted with ❤️ passion</p>
